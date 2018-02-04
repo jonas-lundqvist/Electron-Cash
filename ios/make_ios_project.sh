09da5c283e7779f508c0216c06c787df41455cf6
Calin Culianu <calin.culianu@gmail.com>
2018-02-04 20:39:47 +0200

    made the make_ios_project shell script warn/error the user if they are missing dependencies for make the ios project

--- ios/make_ios_project.sh
+++ ios/make_ios_project.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+/usr/bin/env python3 --version | grep -q " 3.5"
+if [ "$?" != "0" ]; then
+	echo "ERROR: Creating the Briefcase-based Xcode project for iOS requires Python 3.5"
+	exit 1
+fi
+
+/usr/bin/env pip3 show briefcase > /dev/null
+if [ "$?" != "0" ]; then
+	echo "ERROR: Please install briefcase like so: pip3 install briefcase"
+	exit 2
+fi
+
+/usr/bin/env pip3 show cookiecutter > /dev/null
+if [ "$?" != "0" ]; then
+	echo "ERROR: Please install cookiecutter like so: pip3 install cookiecutter"
+	exit 3
+fi
+
 if [ -d iOS ]; then
 	echo "Warning: 'iOS' directory exists. All modifications will be lost if you continue."
 	echo "Continue? [y/N]?"
